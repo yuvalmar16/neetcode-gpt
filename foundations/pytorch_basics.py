77eeedd46e8f452fa6eb8ba8e753a95dd461a9d5
Yuval Margolin <157887231+yuvalmar16@users.noreply.github.com>
2026-07-20 18:01:51 +0300

    Update PyTorch basics (PyTorch 1/4)

--- foundations/pytorch_basics.py
+++ foundations/pytorch_basics.py
@@ -8,8 +8,7 @@ class Solution:
         # Reshape (M, N) tensor to (M*N/2, 2)
         # Use torch.reshape(tensor, new_shape)
         M , N = to_reshape.shape
-        new_rows = (M*N)//2
-        return torch.round(torch.reshape(to_reshape, (new_rows, 2)),decimals=4)
+        return torch.round(torch.reshape(to_reshape, ((M*N)//2, 2)),decimals=4)
 
     def average(self, to_avg: TensorType[float]) -> TensorType[float]:
         # Compute column-wise mean (average across rows)
